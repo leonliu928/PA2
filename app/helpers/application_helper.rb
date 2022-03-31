bf80b556efab3506a75222469113bd2a5cf806a9
EC2 Default User <ec2-user@ip-172-31-63-78.ec2.internal>
2022-03-31 08:15:47 +0000

    part 1 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,6 @@ module ApplicationHelper
     def sortable(column, title = nil)
         title ||= column.titleize
         direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-        css_class = column == sort_column ? "hilite" : nil
-        link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+        link_to title, {:sort => column, :direction => direction}
     end
 end
